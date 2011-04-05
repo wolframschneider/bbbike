000fe1d8006faf7ea3079057e7b6ecc246727e57
Wolfram Schneider <wosch@freebsd.org>
2011-04-05 20:53:59 +0200

    more area map padding

--- html/maps3.js
+++ html/maps3.js
@@ -92,8 +92,8 @@ function bbbike_maps_init (maptype, marker_list, lang, without_area) {
 	    // improve zoom level, max. area as possible
             var bounds_padding = new google.maps.LatLngBounds;
 	    if ( marker_list.length == 2) {
-	  	var padding_x = 0.08; // make the area smaller by this value to cheat to map.getZoom()
-		var padding_y = 0.03;
+	  	var padding_x = 0.10; // make the area smaller by this value to cheat to map.getZoom()
+		var padding_y = 0.07;
 
 	       	bounds_padding.extend(new google.maps.LatLng( marker_list[0][0] + padding_x, marker_list[0][1] + padding_y));
 	       	bounds_padding.extend(new google.maps.LatLng( marker_list[1][0] - padding_x, marker_list[1][1] - padding_y));
