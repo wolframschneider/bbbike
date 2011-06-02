1f8457d61d72177feb22e414083e8bbe24d2dced
Wolfram Schneider <wosch@freebsd.org>
2011-06-02 22:43:49 +0200

    indent

--- html/bbbike.js
+++ html/bbbike.js
@@ -253,8 +253,8 @@ function google_weather(w) {
         if (f.day_of_week) {
             html += f.day_of_week.data;
         }
-	
-	var icon_src = f.icon.data.match(/^http:/) ? f.icon.data : "http://www.google.com" + f.icon.data;
+
+        var icon_src = f.icon.data.match(/^http:/) ? f.icon.data : "http://www.google.com" + f.icon.data;
         html += '<br />' + '<img style="border: 0px solid rgb(187, 187, 204); margin-bottom: 2px;" src="' + icon_src + '" alt="' + f.condition.data + '" title="' + f.condition.data + '" /><br />';
         if (f.high) {
             html += '<nobr>' + celcius(f.high.data) + '°C | ' + celcius(f.low.data) + '°C</nobr>';
