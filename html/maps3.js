5e5e35a4d73c0c4a6a8c53c8a4e7b525efed38de
Wolfram Schneider <wosch@freebsd.org>
2013-05-06 10:10:34 +0000

    save

--- html/maps3.js
+++ html/maps3.js
@@ -3417,11 +3417,9 @@ function format_address(address) {
 
 function googleCodeAddress(address, callback, logger) {
     function log_geocoder(logger, status) {
-        var logger_url = "";
-
         // log geocode requests status by '/cgi/log.cgi';
         if (logger) {
-            logger_url = encodeURI(logger.url + "?type=gmaps-geocoder&city=" + logger.city + "&query=" + address + "&status=" + status);
+            var logger_url = encodeURI(logger.url + "?type=gmaps-geocoder&city=" + logger.city + "&query=" + address + "&status=" + status);
             $.get(logger_url);
         }
     }
